507a47d565928675aec2372e23b2d851238a5452
GereDaniel <geredani9b@gmail.com>
2024-10-07 08:37:55 +0200

    navbar

--- js/main.js
+++ js/main.js
@@ -85,3 +85,16 @@ $(function() {
 	siteMenuClone();
 
 });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
